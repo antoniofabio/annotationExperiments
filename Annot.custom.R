0f5c55d6d7c91b2a9892cfbf422388e1b1324fb7
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2011-01-17 16:12:18 +0100

    add strand info to the custom annotation files

--- Annot.custom.R
+++ Annot.custom.R
@@ -38,7 +38,9 @@ strand <- strand[id]
 Annot.custom <- subset(G, as.character(GeneID) %in% genes)
 rownames(Annot.custom) <- Annot.custom$GeneID
 Annot.custom <- Annot.custom[genes,]
+Annot.custom$strand <- strand
 rownames(Annot.custom) <- names(genes)
-Annot.custom$strand <- strand[match(rownames(Annot.custom), names(genes))]
+Annot.custom$symbol.strand <- with(Annot.custom,
+                                   ifelse(strand == '-', paste(symbol, "(-)"), symbol))
 
 save(Annot.custom, file=outputFile)
